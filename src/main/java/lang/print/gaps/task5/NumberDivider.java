a802c71be14ef4fb0b069407adb5a69b4b627d13
Domantas Venslovas <domantas.venslovas@stud.vilniustech.lt>
2022-08-15 13:25:13 +0300

    hopefully finished task

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float divided=(float) toBeDivided/5F;
+        System.out.println(divided);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        int seconds= (int) Math.floor(minutes)*60;
+        seconds += (int) minutes-Math.floor(minutes);
+        System.out.println(seconds);
     }
 }
